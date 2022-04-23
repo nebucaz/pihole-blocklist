777baf80d62d6ddb38b2b97c02042a7cf000e078
= <nebucaz@pageagent.com>
2022-04-23 11:38:10 +0200

    tests & all scan

--- blocklist/blocklist.py
+++ blocklist/blocklist.py
@@ -1,6 +1,20 @@
 #!/ust/bin/python
-from urllib.parse import urlparse
+from datetime import datetime
+from urllib.parse import urlparse, urlunparse
 import re
+import os
+import sqlite3
+import requests
+import hashlib
+
+# https://pythonawesome.com/a-console-progress-bar-module-for-python/
+import enlighten
+
+from requests.exceptions import Timeout, ConnectionError
+from http.client import RemoteDisconnected
+from os import path
+
+from blocklist.models import Domain
 
 class Blocklist():
     """
@@ -10,13 +24,56 @@ class Blocklist():
     # list of blocklist-files
     adlist = None
     blockLists = []
+    brokenLinks = []
+    blockedDomains = []
+    mydb_path = None
+    
 
-    def __init__(self, adlist = None):
+    def __init__(self, adlist = None, dbPath = None):
         super().__init__()
-
         self.adlist = adlist
 
+        try:
+            if path.exists(self.adlist):
+                self.validateAdList()
+            """
+                with open(self.adlist) as fd:
+                    self.adlist = fd.read()
+            else:
+                self.adlist = adlist
+
+            if self.adlist != None:
+                self.validateAdList()
+            """
+        except:
+            print("no adlist to parse")
+
+        self.mydb_path = ':memory:'
+        if dbPath != None:
+            self.mydb_path = dbPath
+        self.initDB()
+
+    def initDB(self):
+        self.conn = None
+    
+        try:
+            self.conn = sqlite3.connect(self.mydb_path)
+            self.conn.row_factory = sqlite3.Row
+            self.createTables()
+        except sqlite3.Error as err:
+            print("Error Connecting db: {}".format(self.mydb_path))
+            print(err)
+
+    def createTables(self):
+        Domain.initDB(self.conn)
+
+    def getBlacklistDomain(self, name):
+        return Domain(db=self.conn).getByName(name)
+
     def validateAdList(self):
+        self.brokenLinks.clear()
+        self.blockLists.clear()
+
         try:
             with open(self.adlist) as list:
                 for line in list:
@@ -25,15 +82,127 @@ class Blocklist():
                     if re.match('^\s*\#', line):
                         continue
 
-                    url = urlparse(line)
+                    parts = urlparse(line)
                     try:
-                        if url.netloc == None or url.scheme == None:
+                        if parts.netloc == '' or parts.scheme == '':
+                            brokenLink = {"list": self.adlist, "url": line}
+                            self.brokenLinks.append(brokenLink)
                             continue
 
-                        self.blockLists.append(url)
+                        self.blockLists.append(urlunparse(parts))
                     except Exception as e:
                         print(e)
         except IOError as e:
             print(e)
         except Exception as e:
             print(e)
+
+    def updateDomains(self):
+        try:
+            for list in self.blockLists:
+                print("fetching {}".format(list))
+
+                lines = self.fetchList(list)
+                lastH = self.getHash(list)
+                newHash = self.makeHash(lines)
+                if newHash != lastH:
+                    self.writeHash(list, newHash)
+                    self.parseList(lines, list)
+                else:
+                    print("no changes in list")
+
+                #domains = self.parseList(lines)
+                #self.storeDomainsFromList(domains, list)
+        except:
+            print("error while updating {}".format(list))
+
+    # fetch a list 
+    def fetchList(self, url):
+        lines = None
+        try:
+            response = requests.get(url, timeout=30)
+
+            if response.status_code == 200:
+                lines = response.text
+                self.makeHash(lines)
+            else:
+                print("status {} for {}".format(response.status_code, url))
+        except (Timeout, RemoteDisconnected) as e:
+            print("error while fetching {}".format(e))
+
+        return lines
+
+    def getHash(self, listUrl):
+        hash = None
+        hashFileName = self.hashFile(listUrl)
+        try:
+            with open(hashFileName, "r") as fh:
+                hash = fh.readline()
+        except:
+            pass
+        return hash
+
+    def writeHash(self, list, hash):
+        hashFileName = self.hashFile(list)
+        with open(hashFileName, "w") as fh:
+            fh.write(hash)
+
+    def makeHash(self, lines):
+        return hashlib.sha224(lines.encode('utf-8')).hexdigest()
+
+    def hashFile(self, listUrl):
+        url = urlparse(listUrl)
+        return 'db/hash_' + url.hostname + url.path.replace('/', '_') + '.txt'
+
+    # parse a list
+    def parseList(self, lines, sourceList = None):
+        domainList = []
+
+        allLines = lines.splitlines()
+        nLines = len(allLines)
+        pbar = enlighten.Counter(total = nLines, desc='Parsing', unit='lines')
+        for line in allLines:
+            pbar.update()
+            # skip comments
+            if re.match('^\s*\#', line) or line == '' or line == None:
+                continue
+    
+            try:
+                sanitizedDomain = self.getDomain(line)
+                if list != None:
+                    #self.storeDomainUnique(sanitizedDomain)
+                    self.storeDomainUnique(sanitizedDomain, sourceList, 0)
+                else:
+                    domainList.append(sanitizedDomain)
+            except Exception as e: 
+                print(e)
+
+        return domainList
+
+    # throws invalid url
+    # line is one domain per line or in hosts-file format <ip-address> <domain> per line 
+    def getDomain(self, line):
+        parts = line.split(" ")
+        if len(parts) > 1:
+            return parts[1].strip()
+        else:
+            return line.strip()
+
+    def storeDomainsFromList(self, domainList, url):
+        try:
+            for name in domainList:
+                self.storeDomainUnique(name, url, 0)
+        except:
+            print("invalid domain list")
+
+    # store the domain 
+    def storeDomainUnique(self, domain, sourceList, status = 0):
+        created = datetime.now().timestamp()
+        exists = self.getBlacklistDomain(domain)
+        if exists == None or exists.ID == 0:
+            d = Domain(db=self.conn, name=domain, source=sourceList, created=created, status=status)
+            d.save()
+            #print("stored new entry: {}".format(domain))
+            return d
+
+        return exists
--- blocklist/tests/test_Fetcher.py
+++ blocklist/tests/test_Fetcher.py
@@ -1,5 +1,11 @@
 import unittest
+from datetime import datetime
+from sqlite3 import Error
+from os import path, remove
+
 from blocklist.blocklist import Blocklist
+from blocklist.models import Domain
+
 
 class FetcherTest(unittest.TestCase):
     def test_loadEmptyList(self):
@@ -15,8 +21,81 @@ class FetcherTest(unittest.TestCase):
         self.assertEqual(1, len(blocklist.blockLists))
     
     # test list with invalid, not found entries
+    def test_invalidURLs(self):
+        blocklist = Blocklist("blocklist/tests/lists/invalid.list")
+        self.assertIsNotNone(blocklist.adlist)
+        blocklist.validateAdList()
+        self.assertTrue(blocklist.blockLists)
+        self.assertEqual(1, len(blocklist.blockLists))
 
     def test_loadAdList(self):
         blocklist = Blocklist("blocklist/tests/lists/adlist.list")
         self.assertIsNotNone(blocklist.adlist)
         blocklist.validateAdList()
+
+    def test_parseList(self):
+        blocklist = Blocklist()
+        domainList = blocklist.parseList("unique.com\nsalsa.de\nrumba.br\n")
+        self.assertTrue(domainList)
+        self.assertEqual(3, len(domainList))
+
+    def test_getDomain(self):
+        blocklist = Blocklist()
+        domain = blocklist.getDomain("127.0.0.1 unique.com")
+        self.assertEqual("unique.com", domain)
+        domain2 = blocklist.getDomain("adjust.io")
+        self.assertEqual("adjust.io", domain2)
+
+    def test_DomainModel(self):
+        name = "unique.com"
+        blocklist = Blocklist()
+        d = Domain(db = blocklist.conn, name=name, created = datetime.now().timestamp(), status=0, source= "blabla")
+        d.save()
+
+        cur = blocklist.conn.cursor()
+        cur.execute("SELECT * FROM `blacklist` WHERE `DomainName` = ?", [name])
+        domain = cur.fetchone()
+        self.assertIsNotNone(domain)
+        self.assertEqual(name, domain['DomainName'])
+
+        domain2 = Domain(db=blocklist.conn).getByName("unique.com")
+        self.assertIsNotNone(domain2)
+        self.assertEqual(name, domain2.DomainName)
+
+    def test_databaseFile(self):
+        if path.exists("db/test.db"):
+            remove("db/test.db")
+    
+        blocklist = Blocklist(dbPath="db/test.db")
+        self.assertTrue(path.exists("db/test.db"))
+
+    def test_storeUnique(self):
+        name = "unique.com"
+        blocklist = Blocklist()
+        blocklist.storeDomainUnique(name, sourceList="a")
+        blocklist.storeDomainUnique(name, sourceList="b")
+
+        cur = blocklist.conn.cursor()
+        cur.execute("SELECT count(*) FROM `blacklist` WHERE `DomainName` = ?", [name])
+        count = cur.fetchone()[0]
+        self.assertEqual(1, count)
+
+    def test_fetchList(self):
+        blocklist = Blocklist()
+        lines = blocklist.fetchList("https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt")
+        self.assertIsNotNone(lines)
+        domainList = blocklist.parseList(lines)
+        self.assertEqual(34, len(domainList))
+
+    def test_storeDomainsFromList(self):
+        blocklist = Blocklist() #Blocklist(dbPath="db/test.db")
+        blocklist.conn.execute("delete from blacklist")
+        lines = blocklist.fetchList("https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt")
+        self.assertIsNotNone(lines)
+        domainList = blocklist.parseList(lines)
+        blocklist.storeDomainsFromList(domainList, "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt")
+
+        cur = blocklist.conn.cursor()
+        cur.execute("select count(*) from blacklist")
+        count = cur.fetchone()[0]
+        self.assertEqual(len(domainList), count)
